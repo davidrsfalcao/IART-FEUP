29f7dcc02fbdab58610e837e33c4b3eb28ac467d
Claudia Rodrigues <claudiacatarina232@gmail.com>
2018-05-03 16:57:05 +0100

    started search

--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -1,3 +1,4 @@
+import elements.People;
 import elements.Vehicle;
 import graph.Graph;
 import graph.Point;
@@ -5,7 +6,10 @@ import graph.Route;
 import graph.State;
 import utils.Utils;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+
+import static java.lang.Thread.sleep;
 
 public class Search {
 
@@ -17,27 +21,67 @@ public class Search {
         iterate(newState);    //inicialmente, apenas utiliza 1 veiculo
     }
 
-    public static void iterate(State st) {
+    public static boolean iterate(State st) {
 
         Vehicle v = st.getVehicles().get(0);
-        Point p = v.getLocation();
+        String currentPoint = v.getLocation();
 
-        if(st.alreadyVisited(p.getName())){
-            System.out.println("The vehicle is "+p.getName());
-            System.out.println("Current path: "+st.getPath());
-            return;
+        ArrayList<People> people = st.getPeople();
+        for(People pp : people){
+            if(pp.getLocation().equals(currentPoint) && pp.getNumber()>0){
+
+                //rescue the people !!
+
+                int nPeople = pp.getNumber();
+                int maxCap = v.getCapacity();
+
+                if(maxCap >= nPeople){
+                    pp.setNumber(0);
+                    v.setCapacity(maxCap-nPeople);
+                }
+                else{
+                    pp.setNumber(nPeople-maxCap);
+                    v.setCapacity(0);
+                }
+
+                st.clearPath();
+            }
         }
 
-        st.addToPath(p.getName());
+        if(st.getGraph().getSafe_point().getName().equals(currentPoint)){
+            System.out.println("In final state!");
+            System.out.println(st.toString());
+            return true;
+        }
 
-        Point search = Utils.getPointByName(p.getName(), st.getGraph().getPoints());
+
+        st.displayState();
+        try {
+            sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<String> path = st.getPath();
+
+        if(path.contains(currentPoint)){
+            System.out.println("The vehicle is in: "+ currentPoint);
+            System.out.println("Current path: "+st.getPath());
+            return false;
+        }
+
+        path.add(currentPoint);
+
+        Point search = Utils.getPointByName(currentPoint, st.getGraph().getPoints());
         ArrayList<Route> routes = search.getRoutes();
 
         for (Route r : routes) {
             Point dest = r.getDestiny();
-            v.setLocation(dest);
-            iterate(new State(st));
+            v.setLocation(dest.getName());
+            if (iterate(new State(st))) return true;
         }
+
+        return false;
 
     }
 }
