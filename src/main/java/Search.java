b19c8766affef566218f4a9bbed57b71fed736b2
Claudia Rodrigues <claudiacatarina232@gmail.com>
2018-05-13 22:07:43 +0100

    started astar

--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -1,3 +1,5 @@
+import com.ctc.wstx.util.InternCache;
+import com.sun.javafx.scene.traversal.Algorithm;
 import elements.People;
 import elements.Vehicle;
 import graph.Point;
@@ -5,14 +7,19 @@ import graph.Route;
 import graph.State;
 import utils.Utils;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.SortedMap;
+import java.util.*;
 
 
 public class Search {
 
+    public enum ALGORITHM {
+        DFS, ASTAR
+    }
+
+
     public static ArrayList<State> solutions;
+
+    public static TreeMap<Double, State> states;
 
     /*
      * dfs search
@@ -20,7 +27,7 @@ public class Search {
     public static void dfs(State st) {
 
         solutions = new ArrayList<State>();
-        iterate(st);
+        iterate(st, ALGORITHM.DFS);
 
         if (solutions.size() == 0) {
             System.out.println("Solution not found!");
@@ -30,7 +37,33 @@ public class Search {
             System.out.println(solutions.get(0).printStats());
             System.out.println("end");*/
 
-            for(State s : solutions){
+            for (State s : solutions) {
+                System.out.println(s.printStats());
+            }
+        }
+        System.out.println("end");
+
+    }
+
+    /*
+     * dfs search
+     * */
+    public static void a_star(State st) {
+
+        solutions = new ArrayList<State>();
+        states = new TreeMap<Double, State>();
+
+        iterate(st, ALGORITHM.ASTAR);
+
+        if (solutions.size() == 0) {
+            System.out.println("Solution not found!");
+        } else {
+
+            /*Collections.sort(solutions);
+            System.out.println(solutions.get(0).printStats());
+            System.out.println("end");*/
+
+            for (State s : solutions) {
                 System.out.println(s.printStats());
             }
         }
@@ -77,7 +110,7 @@ public class Search {
                 }
 
                 v.clearPath();
-
+                v.setReturn();
             }
         }
     }
@@ -103,6 +136,7 @@ public class Search {
             } else {
 
                 v.clearPath();
+                v.setGo();
 
             }
 
@@ -110,6 +144,8 @@ public class Search {
                 v.stop();               //stop the vehicle
                 st.stopCountingVehicle(v);
             }
+
+
         }
         return false;
     }
@@ -120,23 +156,20 @@ public class Search {
      */
     public static boolean checkState(Vehicle v, State st) {
 
-        if (v.isActive()) {
+        String currentPoint = v.getLocation();
 
-            String currentPoint = v.getLocation();
+        if (!checkValidState(v)) {
+            return true;            //invalid state
+        }
 
-            if (!checkValidState(v)) {
-                return true;            //invalid state
-            }
+        checkRescuePeople(v, st);
 
-            checkRescuePeople(v, st);
+        if (checkSafePlace(v, st)) {
+            solutions.add(st);
+            return true;            //found solution
 
-            if (checkSafePlace(v, st)) {
-                solutions.add(st);
-                return true;            //found solution
-
-            } else {
-                v.getPath().add(currentPoint);
-            }
+        } else {
+            v.getPath().add(currentPoint);
         }
 
         return false;
@@ -145,16 +178,76 @@ public class Search {
     /*
      * Computes the next states
      */
-    public static void next_states(State st, ArrayList<Vehicle> vehicles) {
 
-        int vh_index = st.getNextVehicle();
-        Vehicle vh = vehicles.get(vh_index);
+    /*
+            //ordena rotas de acordo com heuristica
 
-        // check this vehicle as moved
-        Boolean[] moved_vehicles = st.getMovedVehicles();
-        moved_vehicles[vh_index] = true;
-        st.setLastMoved(vh_index);
+            */
 
+
+    //https://stackoverflow.com/questions/16869920/a-heuristic-calculation-with-euclidean-distance
+    public static double calculate_distance(Point destination, Point currentNode) {
+
+        double x = Math.pow(destination.getX() - currentNode.getX(), 2.0);
+        double y = Math.pow(destination.getX() - currentNode.getX(), 2.0);
+
+        return x + y;
+    }
+
+
+    public static void addNewStates(Vehicle vh, int vh_index, State st, ArrayList<Route> routes) {
+
+        //Heuristic - closer to destiny
+
+        String loc = st.getPeople().get(0).getLocation();
+        Point dest = Utils.getPointByName(loc, st.getGraph().getPoints());
+
+        // add all the possible next states
+        for (Route r : routes) {
+
+            double res = calculate_distance(r.getDestiny(), dest);
+
+            //add state with the corresponding cost
+            vh.setLocation(r.getDestiny().getName());
+            vh.addDistance(r.getDistance());
+
+            if (vh_index == st.getIndexCount()) {
+                st.addTime(r.getDistance());
+            }
+
+            states.put(res, new State(st));
+
+            if (vh_index == st.getIndexCount()) {
+                st.removeTime(r.getDistance());
+            }
+
+            vh.removeDistance(r.getDistance());
+
+        }
+    }
+
+
+    public static int compute_astar(State st, Vehicle vh, int vh_index, ArrayList<Vehicle> vehicles) {
+
+        if (vh.isActive()) {
+
+            String currentPoint = vh.getLocation();
+            Point search = Utils.getPointByName(currentPoint, st.getGraph().getPoints());
+            ArrayList<Route> routes = search.getRoutes();
+
+
+            addNewStates(vh, vh_index, st, routes);
+            return 0;
+
+        } else {
+
+           // next_states(st, vehicles, ALGORITHM.ASTAR);  //the vehicle is in the same location
+            return 1;
+        }
+    }
+
+
+    public static void compute_dfs(State st, Vehicle vh, int vh_index, ArrayList<Vehicle> vehicles) {
 
         if (vh.isActive()) {
 
@@ -171,7 +264,7 @@ public class Search {
                     st.addTime(r.getDistance());
                 }
 
-                iterate(new State(st));
+                iterate(new State(st), ALGORITHM.DFS);
 
                 if (vh_index == st.getIndexCount()) {
                     st.removeTime(r.getDistance());
@@ -181,17 +274,47 @@ public class Search {
 
             }
 
+        } else {
+
+            next_states(st, vehicles, ALGORITHM.DFS);  //the vehicle is in the same location
+
         }
-        else{
+    }
 
-            next_states(st, vehicles);  //the vehicle is in the same location
+    public static void next_states(State st, ArrayList<Vehicle> vehicles, ALGORITHM alg) {
 
+        int vh_index = st.getNextVehicle();
+        Vehicle vh = vehicles.get(vh_index);
+
+        // check this vehicle as moved
+        Boolean[] moved_vehicles = st.getMovedVehicles();
+        moved_vehicles[vh_index] = true;
+        st.setLastMoved(vh_index);
+
+
+        if (alg == ALGORITHM.DFS) {
+
+            compute_dfs(st, vh, vh_index, vehicles);
+
+        } else if (alg == ALGORITHM.ASTAR) {
+
+            if( compute_astar(st, vh, vh_index, vehicles) ==0 ){
+
+                if (states.size() != 0) {
+                    State next = states.remove(states.firstKey());
+                    iterate(next, ALGORITHM.ASTAR);
+                }
+
+            }
+            else{
+                next_states(st, vehicles, ALGORITHM.ASTAR);  //the vehicle is in the same location
+            }
         }
 
     }
 
 
-    public static void iterate(State st) {
+    public static void iterate(State st, ALGORITHM alg) {
 
 
         ArrayList<Vehicle> vehicles = st.getVehicles();
@@ -203,7 +326,21 @@ public class Search {
             Vehicle v = vehicles.get(toMove);
 
             if (checkState(v, st)) {        // no more follow states
-                return;
+
+                if (alg == ALGORITHM.DFS) {
+                    return;
+                } else {   //chose another state
+
+                    if (states.size() != 0) {
+
+                        State next = states.remove(states.firstKey());
+
+                        iterate(next, ALGORITHM.ASTAR);
+                        return;
+                    }
+
+                }
+
             }
         } else {
 
@@ -215,8 +352,9 @@ public class Search {
 
         }
 
-        next_states(st, vehicles);          // create follow states
+        next_states(st, vehicles, alg);          // create follow states
 
     }
+
 
 }
--- src/main/java/elements/Vehicle.java
+++ src/main/java/elements/Vehicle.java
@@ -5,10 +5,15 @@ import graph.Route;
 import java.util.ArrayList;
 
 public class Vehicle {
+
+    public enum State {
+        GO, RETURN
+    }
+
     private String name;
     private int capacity;
     private int currentPersons;
-    private int totalRescued=0;
+    private int totalRescued = 0;
     private int velocity;
     private String location;
     private ArrayList<String> goPath;
@@ -16,6 +21,7 @@ public class Vehicle {
     private float totalTime;
 
     private boolean active;
+    private State state;
 
 
     public Vehicle(String name, int capacity, int velocity, String location) {
@@ -23,28 +29,30 @@ public class Vehicle {
         this.capacity = capacity;
         this.velocity = velocity;
         this.location = location;
-        this.currentPersons=0;
-        this.totalTime=0;
+        this.currentPersons = 0;
+        this.totalTime = 0;
         this.goPath = new ArrayList<String>();
         this.returnPath = new ArrayList<String>();
 
-        this.totalRescued=0;
-        this.active=true;
+        this.totalRescued = 0;
+        this.active = true;
+        state = State.GO;
     }
 
 
-    public Vehicle(Vehicle vehicle){
-        this.name=vehicle.name;
+    public Vehicle(Vehicle vehicle) {
+        this.name = vehicle.name;
         this.capacity = vehicle.capacity;
-        this.currentPersons=vehicle.currentPersons;
-        this.velocity=vehicle.velocity;
-        this.location=vehicle.location;
-        this.totalTime=vehicle.totalTime;
-        this.goPath=new ArrayList<String>(vehicle.getPath());
-        this.returnPath=new ArrayList<String>(vehicle.getReturnPath());
+        this.currentPersons = vehicle.currentPersons;
+        this.velocity = vehicle.velocity;
+        this.location = vehicle.location;
+        this.totalTime = vehicle.totalTime;
+        this.goPath = new ArrayList<String>(vehicle.getPath());
+        this.returnPath = new ArrayList<String>(vehicle.getReturnPath());
 
         this.totalRescued = vehicle.totalRescued;
-        this.active=vehicle.active;
+        this.active = vehicle.active;
+        this.state = vehicle.state;
     }
 
 
@@ -54,15 +62,25 @@ public class Vehicle {
         return name;
     }
 
-    public boolean isActive(){ return active; }
+    public boolean isActive() {
+        return active;
+    }
 
-    public void stop(){ this.active=false;}
+    public void stop() {
+        this.active = false;
+    }
 
-    public int getTotalRescued() { return totalRescued; }
+    public int getTotalRescued() {
+        return totalRescued;
+    }
 
-    public int getCapacity() {  return capacity;  }
+    public int getCapacity() {
+        return capacity;
+    }
 
-    public float getTime() { return this.totalTime; }
+    public float getTime() {
+        return this.totalTime;
+    }
 
     public int getVelocity() {
         return velocity;
@@ -73,18 +91,27 @@ public class Vehicle {
     }
 
 
+    public ArrayList<String> getReturnPath() {
+        return returnPath;
+    } //saved path
 
-    public ArrayList<String> getReturnPath() { return returnPath; } //saved path
-
-    public ArrayList<String> getPath() { return goPath; }   //current path
+    public ArrayList<String> getPath() {
+        return goPath;
+    }   //current path
 
     /* setters */
 
-    public void setCapacity(int capacity) { this.capacity = capacity; }
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 
-    public void addDistance(int distance ){ this.totalTime+= distance; }
+    public void addDistance(int distance) {
+        this.totalTime += distance;
+    }
 
-    public void removeDistance(int distance) { this.totalTime -= distance; }
+    public void removeDistance(int distance) {
+        this.totalTime -= distance;
+    }
 
     public void setLocation(String location) {
         this.location = location;
@@ -95,47 +122,67 @@ public class Vehicle {
      * Save the current path and clear
      */
     public void clearPath() {
-        ArrayList<String> newList =new ArrayList<String>(this.goPath);
+        ArrayList<String> newList = new ArrayList<String>(this.goPath);
         this.returnPath.addAll(newList);
-        this.goPath=new ArrayList<String>();
+        this.goPath = new ArrayList<String>();
     }
 
     /*
      * Check if the vehicle can transport n persons
      * */
-    public boolean canTransport(int number){ return capacity-number>=0; }
+    public boolean canTransport(int number) {
+        return capacity - number >= 0;
+    }
 
     /*
      * Check if the vehicle has people
      * */
-    public boolean isTransporting(){  return currentPersons!=0;  }
+    public boolean isTransporting() {
+        return currentPersons != 0;
+    }
 
     /*
-    * Update current persons in the vehicle
-    * */
-    public void setCurrentPersons(int number){
-        this.currentPersons=number;
-        this.capacity= capacity-number;
+     * Update current persons in the vehicle
+     * */
+    public void setCurrentPersons(int number) {
+        this.currentPersons = number;
+        this.capacity = capacity - number;
     }
 
     /*
      * Leave people in the safe place
      * */
-    public int emptyVehicle(){
-        this.capacity=capacity+currentPersons;
+    public int emptyVehicle() {
+        this.capacity = capacity + currentPersons;
         int rescued = currentPersons;
-        this.currentPersons=0;
-        this.totalRescued+=rescued;
+        this.currentPersons = 0;
+        this.totalRescued += rescued;
         return rescued;
     }
 
     /*
      * Print vehicle status
      * */
-    public String toString(){
-        return this.name+" with "+this.currentPersons+" people; Time: "+this.totalTime + " Path: "+
-        (returnPath.isEmpty()?"":returnPath.toString()) +
-                (goPath.isEmpty()?"":goPath.toString());
+    public String toString() {
+        return this.name + " with " + this.currentPersons + " people; Time: " + this.totalTime + " Path: " +
+                (returnPath.isEmpty() ? "" : returnPath.toString()) +
+                (goPath.isEmpty() ? "" : goPath.toString());
+    }
+
+
+    public String getState() {
+        return state.name();
+    }
+
+    public void setReturn(){
+
+        this.state = State.RETURN;
+
+    }
+    public void setGo(){
+
+        this.state = State.GO;
+
     }
 
 }
